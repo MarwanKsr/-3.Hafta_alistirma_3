ddba3103d3d8435002457a4ff4590716c0ed7d49
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-08 01:19:32 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace alistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayiyi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            if (sayi % 2 == 1)
+            {
+                sayi*=2;
+            }
+            else
+            {
+                sayi /= 2;
+            }
+            Console.WriteLine(sayi);
+        }
+    }
+}
